1a6a6acfb86b1824c2f11d981b960552a93e2d77
mantis-gdn <info@gigdates.net>
2025-01-16 21:21:05 -0500

    Update CORS policy to use HTTPS for gigdates.net

--- netlify/functions/track.js
+++ netlify/functions/track.js
@@ -15,7 +15,7 @@ exports.handler = async (event) => {
     return {
       statusCode: 200,
       headers: {
-        "Access-Control-Allow-Origin": "gigdates.net", // Allow requests from gigdates.net
+        "Access-Control-Allow-Origin": "https://gigdates.net", // Allow requests from https://gigdates.net
         "Access-Control-Allow-Headers": "Content-Type",
         "Access-Control-Allow-Methods": "POST, OPTIONS",
       },
@@ -28,7 +28,7 @@ exports.handler = async (event) => {
     return {
       statusCode: 405,
       headers: {
-        "Access-Control-Allow-Origin": "gigdates.net", // Allow requests from gigdates.net
+        "Access-Control-Allow-Origin": "https://gigdates.net", // Allow requests from https://gigdates.net
         "Access-Control-Allow-Headers": "Content-Type",
       },
       body: "Method Not Allowed",
@@ -51,7 +51,7 @@ exports.handler = async (event) => {
     return {
       statusCode: 200,
       headers: {
-        "Access-Control-Allow-Origin": "gigdates.net", // Allow requests from gigdates.net
+        "Access-Control-Allow-Origin": "https://gigdates.net", // Allow requests from https://gigdates.net
         "Access-Control-Allow-Headers": "Content-Type",
       },
       body: "Event logged successfully",
@@ -61,7 +61,7 @@ exports.handler = async (event) => {
     return {
       statusCode: 500,
       headers: {
-        "Access-Control-Allow-Origin": "gigdates.net", // Allow requests from gigdates.net
+        "Access-Control-Allow-Origin": "https://gigdates.net", // Allow requests from https://gigdates.net
         "Access-Control-Allow-Headers": "Content-Type",
       },
       body: "Internal Server Error",
